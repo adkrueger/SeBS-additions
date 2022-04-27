2323be14f83ca6e1fd96249f4ad2ddd55f57f1a5
adkrueger <adkrueger@wpi.edu>
2022-04-27 02:18:02 +0000

    adding stuff to get large input to work on stateless

--- benchmarks/600.additional/610.stateless/input.py
+++ benchmarks/600.additional/610.stateless/input.py
@@ -1,7 +1,7 @@
 arr_dim1s = {
 	'test': 10,
 	'small': 400,
-	'large': 1000
+	'large': 500
 }
 
 arr_dim2s = {
@@ -13,7 +13,7 @@ arr_dim2s = {
 arr_dim3s = {
 	'test': 8,
         'small': 300,
-        'large': 750
+        'large': 500
 }
 
 
@@ -22,5 +22,5 @@ def buckets_count():
 	return (0,0)
 
 def generate_input(data_dir, size, input_buckets, output_buckets, upload_func):
-	return { 'arr_dim1': arr_dim1s[size], 'arr_dim2': arr_dim2s[size], 'arr_dim3': arr_dim3s[size], 'num_loops': 50 }
+	return { 'arr_dim1': arr_dim1s[size], 'arr_dim2': arr_dim2s[size], 'arr_dim3': arr_dim3s[size], 'num_loops': 20 }
 
