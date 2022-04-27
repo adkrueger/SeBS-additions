37fb7add683b8d90d413d211582eb8d51fc1cae2
adkrueger <adkrueger@wpi.edu>
2022-04-27 14:36:40 -0400

    adding labels when plotting

--- bench_results/plotting.py
+++ bench_results/plotting.py
@@ -20,6 +20,26 @@ def plot_exec_time_metrics(file_name, show_plots=False):
     # 256NB iterations
     plot_exec_time_helper(file_no_ext, warm256_iters, cold256_iters, 256, show_plots=show_plots)
 
+# helper for plotting execution times; creates box plots of cold vs. warm executions
+def plot_exec_time_helper(plot_title, warm_df, cold_df, mem_size, show_plots=False):
+    fig = plt.figure(figsize =(10, 7))
+    ax = fig.add_subplot(111)
+    
+    # grab the execution time columns
+    warm_exec_time = warm_df["exec_time"] / 1000000 # convert to seconds
+    cold_exec_time = cold_df["exec_time"] / 1000000
+    plt.boxplot([warm_exec_time, cold_exec_time])
+    title = plot_title + ": Exec. Time (" + str(mem_size) + "MB)"
+    plt.title(title)
+    ax.set_xticklabels(["Warm", "Cold"])    # make sure the boxes are labelled properly
+    plt.xlabel("Application Setting")
+    plt.ylabel("End-to-End Latency (s)")
+
+    if show_plots:
+        plt.show()
+
+    plt.savefig("plots/" + plot_title + "_execTime_" + str(mem_size))
+
 # plots a bar chart of average memory usage across all benchmarks
 def plot_mems(file_names, show_plots=False):
     fig = plt.figure(figsize =(10, 7))
@@ -30,39 +50,24 @@ def plot_mems(file_names, show_plots=False):
     # for each file, read the file, get its average memory usage, and keep track of its name
     for f in file_names:
         curr_df = pd.read_csv(f, header=0)
-        print(f"curr_df: {curr_df}")
+        # print(f"curr_df: {curr_df}")
         all_mems.append(np.mean(curr_df["mem_used"].dropna().to_numpy()))
         all_file_names.append(os.path.splitext(f)[0])
-    print(f"all mems: {all_mems}")
-    print(f"all_file_names: {all_file_names}")
+    # print(f"all mems: {all_mems}")
+    # print(f"all_file_names: {all_file_names}")
     plt.bar(all_file_names, all_mems)
+    plt.xlabel("Application Ran")
+    plt.ylabel("Memory Usage (MB)")
+
     plt.title("Memory Usage Across Different Applications")
     
     if show_plots:
         plt.show()
     plt.savefig("plots/MemoryUsage")
 
-# helper for plotting execution times; creates box plots of cold vs. warm executions
-def plot_exec_time_helper(plot_title, warm_df, cold_df, mem_size, show_plots=False):
-    fig = plt.figure(figsize =(10, 7))
-    ax = fig.add_subplot(111)
-    
-    # grab the execution time columns
-    warm_exec_time = warm_df["exec_time"]
-    cold_exec_time = cold_df["exec_time"]
-    plt.boxplot([warm_exec_time, cold_exec_time])
-    title = plot_title + ": Exec. Time (" + str(mem_size) + "MB)"
-    plt.title(title)
-    ax.set_xticklabels(["Warm", "Cold"])    # make sure the boxes are labelled properly
-
-    if show_plots:
-        plt.show()
-
-    plt.savefig("plots/" + plot_title + "_execTime_" + str(mem_size))
-
 if __name__=="__main__":
     files = []
     files += [f for f in os.listdir() if os.path.isfile(f) and f != "plotting.py"]
-    # for f in files:
-    #     plot_exec_time_metrics(f, False)
+    for f in files:
+        plot_exec_time_metrics(f, False)
     plot_mems(files)
